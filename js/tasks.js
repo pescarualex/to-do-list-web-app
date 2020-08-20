362b950725c0e30e9f3ba4cdaad345cf421f65d8
pescarualex <pescarualexflorin@gmail.com>
2020-08-20 19:35:45 +0300

    implemented update and delete task

--- js/tasks.js
+++ js/tasks.js
@@ -32,6 +32,30 @@ window.ToDoList = {
         });
     },
 
+    updateTask: function (id, done) {
+        let body = {
+            "done": done
+        };
+
+        $.ajax({
+            url:ToDoList.API_URL + '?id=' + id,
+            method: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify(body)
+        }).done(function(){
+                ToDoList.getTasks();
+        })
+    },
+
+    deleteTask: function (id) {
+        $.ajax({
+            url:ToDoList.API_URL + '?id=' + id,
+            method: 'DELETE'
+        }).done(function (){
+            ToDoList.getTasks();
+        })
+    },
+
     getTaskRow: function (task) {
 
         let formatedDeadline = new Date(...task.deadline).toLocaleDateString("ro");
@@ -61,6 +85,23 @@ window.ToDoList = {
             event.preventDefault();
 
             ToDoList.createTask();
+        });
+
+        $('#tasks').delegate('.mark-done', 'change', function (event) {
+            event.preventDefault();
+
+            const id = $(this).data('id');
+            const checkboxChecked = $(this).is(':checked');
+
+            ToDoList.updateTask(id, checkboxChecked);
+        });
+
+        $('#tasks').delegate('.delete-link', 'click', function (event) {
+            event.preventDefault();
+
+            const id = $(this).data('id');
+
+            ToDoList.deleteTask(id);
         });
     }
 };
